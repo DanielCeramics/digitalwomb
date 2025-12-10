cf28613c85afab1d5f3a37a1f20c1ac1359ead54
DanielSargent <danielceramicart@gmail.com>
2025-12-10 10:45:42 -0600

    Name feature; tweaks

--- app.js
+++ app.js
@@ -27,24 +27,65 @@ const gifids = [
   },
 ]
 
+const namegarden = [
+  {
+    name: 'Rosemary',
+    image: 'static/names/Rosemary_Name.png'
+  },
+  {
+    name: 'Ladylike',
+    image: 'static/names/Ladylike_Name.png'
+  },
+  {
+    name: 'Eleanor',
+    image: 'static/names/Eleanor_Name.png'
+  },
+  {
+    name: 'Ne',
+    image: 'static/names/Ne_Name.png'
+  },
+  {
+    name: 'Star',
+    image: 'static/names/Star_Name.png'
+  },
+  {
+    name: 'Jazz',
+    image: 'static/names/Jazz_Name.png'
+  },
+]
 
 document.addEventListener('DOMContentLoaded', () => {
   const friends = document.getElementsByClassName("friend");
+  const names = document.getElementsByClassName("name");
 
   var friendidbank = [];
   const viewport = document.getElementById("viewportimage");
+  const displayname = document.getElementById("displayname");
+
 
   for (i=0; i<friends.length; i++) {
     var friendid = [];
     friendid.push(friends[i].innerText);
     friendidbank.push(friendid);
     friends[i].onmouseover = function () {
-      var currentname = this.innerText;
+      var currentfriend = this.innerText;
       for (i=0; i<gifids.length; i++) {
         console.log(gifids[i].name);
-        console.log(currentname);
-        if (currentname == gifids[i].name) {
+        console.log(currentfriend);
+        if (currentfriend == gifids[i].name) {
           viewport.src = gifids[i].gif;
+        }
+      }
+    };
+  }
+
+  for (i=0; i<names.length; i++) {
+    names[i].onmouseover = function () {
+      var currentname = this.id;
+      console.log(currentname);
+      for (i=0; i<namegarden.length; i++) {
+        if (currentname == namegarden[i].name) {
+          displayname.src = namegarden[i].image;
         }
       }
     };
